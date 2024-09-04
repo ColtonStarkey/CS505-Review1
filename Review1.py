1300e6c282717cb162b41ba28476fb2456b8caed
Colton <cdstarkey1s@semo.edu>
2024-09-04 18:28:22 -0500

    Finished all comb and perm functions

--- Review1.py
+++ Review1.py
@@ -8,6 +8,7 @@ def func1():
     matrix_b = [[3.1234,3.0987,3.1234],[2.1111,3.2222,3.3333],[1.0987,1.3456,5.1234]]
     matrix_c = [[3.1243,3.0989,3.1256],[2.6721,3.6785,3.9017],[1.1254,2.8956,5.9187]]
 
+print("1. i")
 print(timeit.timeit(func1, number=1000))
 
 #    ii)	Write a Python program and NumPy
@@ -15,20 +16,20 @@ def func2():
     np_matrix_a = np.matrix([[3.7827,3.3454,3.2341],[2.2122,3.5678,3.9087],[1.1234,2.8934,5.9087]])
     np_matrix_b = np.matrix([[3.1234,3.0987,3.1234],[2.1111,3.2222,3.3333],[1.0987,1.3456,5.1234]])
     np_matrix_c = np.matrix([[3.1243,3.0989,3.1256],[2.6721,3.6785,3.9017],[1.1254,2.8956,5.9187]])
-
+print("1. ii")
 print(timeit.timeit(func2, number=1000))
 
 # 2)	Write a Python Program and perform the following operations ?
 
 # I.	Create a List {1225, 4986, 6789, 7890, 2345, 6783, 0987, 1234, 8765, 3456}
-print("I")
+print("2. I")
 def funci():
     list1 = (1225, 4986, 6789, 7890, 2345, 6783, 987, 1234, 8765, 3456)
 
 print(timeit.timeit(funci, number=1000))
 
 # II.	Iterate using a for loop
-print("II")
+print("2. II")
 def funcii():
     list1 = (1225, 4986, 6789, 7890, 2345, 6783, 987, 1234, 8765, 3456)
 
@@ -38,7 +39,7 @@ def funcii():
 print(timeit.timeit(funcii, number=1000))
 
 # III.	Iterate using for loop and range
-print("III")
+print("2. III")
 def funciii():
     list1 = (1225, 4986, 6789, 7890, 2345, 6783, 987, 1234, 8765, 3456)
 
@@ -48,7 +49,7 @@ def funciii():
 print(timeit.timeit(funciii, number=1000))
 
 # IV.	List Comprehension
-print("IV")
+print("2. IV")
 def funciv():
     list1 = (1225, 4986, 6789, 7890, 2345, 6783, 987, 1234, 8765, 3456)
     return [i for i in list1]
@@ -56,7 +57,7 @@ def funciv():
 print(timeit.timeit(funciv, number=1000))
 
 # V.	Enumerate
-print("V")
+print("2. V")
 def funcv():
     list1 = (1225, 4986, 6789, 7890, 2345, 6783, 987, 1234, 8765, 3456)
     enum1 = enumerate(list1)
@@ -66,7 +67,7 @@ def funcv():
 print(timeit.timeit(funcv, number=1000))
 
 # VI.	Iter function and next function
-print("VI")
+print("2. VI")
 def funcvi():
     list1 = (1225, 4986, 6789, 7890, 2345, 6783, 987, 1234, 8765, 3456)
     iter1 = iter(list1)
@@ -76,7 +77,7 @@ def funcvi():
 print(timeit.timeit(funcvi, number=1000))
 
 # VII.	Map function
-print("VII")
+print("2. VII")
 def funcvii():
     list1 = (1225, 4986, 6789, 7890, 2345, 6783, 987, 1234, 8765, 3456)
     def print_test(x):
@@ -88,7 +89,7 @@ def funcvii():
 print(timeit.timeit(funcvii, number=1000))
 
 # VIII.	Using zip
-print("VIII")
+print("2. VIII")
 def funcviii():
     list1 = (1225, 4986, 6789, 7890, 2345, 6783, 987, 1234, 8765, 3456)
     zipped = zip(list1,[0,1,2,3,4,5,6,7,8,9])
@@ -98,7 +99,7 @@ def funcviii():
 print(timeit.timeit(funcviii, number=1000))
 
 # IX.	Using NumPy Module
-print("IX")
+print("2. IX")
 def funcix():
     arr = np.array([1225, 4986, 6789, 7890, 2345, 6783, 987, 1234, 8765, 3456])
     return(arr)
@@ -123,10 +124,36 @@ def allperms(my_list):
         for each in allperms(leftover):
             total.append([temp]+each)
     return total
-
-for i in allperms(test_list):
-    print(i)
+print("3. Permutations")
+print(timeit.timeit(lambda: allperms(test_list), number=1000))
 
 def allcomb(my_list):
-    
+    total = [[]]
+    for i in my_list:
+        for j in total.copy():
+            comb = j + [i]
+            if comb != []:
+                total.append(comb)
+    total.pop(0)
+    return total
+print("3. Combinations")
+print(timeit.timeit(lambda: allcomb(test_list), number=1000))
 
+import itertools
+
+def iter_perms(my_list):
+    perm = list(itertools.permutations(test_list))
+    return perm
+
+print("4. Permutations")
+print(timeit.timeit(lambda: iter_perms(test_list), number=1000))
+
+def iter_comb(my_list, r):
+    total = []
+    for each in range(1,r+1):
+        combo = list(itertools.combinations(test_list, each))
+        total.append(combo)
+    return total
+
+print("4. Combinations")
+print(timeit.timeit(lambda: iter_comb(test_list, 5), number=1000))
